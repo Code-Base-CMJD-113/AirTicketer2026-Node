581eea5c0bed64de9cf9ea8ce35d07c9feae5cc5
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-22 13:15:54 +0530

    Create userRoutes and impls are ongoing

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const router = express.Router()
+const authUrl = "/auth"
+const User = require("../model/userModel")
+const bcrypt  = require("bcryptjs")
+const { addUser } = require("../service/authService")
+
+//SignUp
+router.post(`${authUrl}/signup`,async(req,res)=>{
+    const { 
+        fullName, 
+        email, 
+        password, 
+        role,
+         phone } = req.body
+        
+         if(!fullName || !email || !password || !role || !phone){
+             return res.status(401).json({message:"Missing required field/s"})
+         }
+         try{
+            const user = addUser(req.body)
+            // To be generate the token
+         }catch(err){
+             console.error(err)
+         }
+});
+
+//Sign in
+ router.post(`${authUrl}/signin`,async(req,res)=>{
+     const { email, password} = req.body
+     const user = User.findOne({email})
+     //check pw validity
+     const isValidPassword = await bcrypt.compare(password, user.password)
+     if(!user || !isValidPassword){
+        return res.status(401).json({message:"Invalid credentials"})
+     }
+     // To be generate the token
+
+ })
