dafa81713e898c37796d317fd266be4c79e8d12e
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-22 10:03:33 +0530

    Create airport Schema

--- src/model/airportModel.js
+++ src/model/airportModel.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose")
+const { v4: uuid4} = require("uuid")
 
 const airportSchema = new mongoose.Schema({
-    
-})
+    airportCode:{
+        type: String,
+        default: () => uuid4(),
+        unique: true
+    },
+    name: {type: String, required: true},
+    city: {type: String, required: true},
+    country: {type: String, required: true}
+});
+
+module.exports = mongoose.model("airports",airportSchema)
