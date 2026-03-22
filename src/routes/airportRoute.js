41d8020a3659c87ca55132dbad6615486ed32c80
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-22 09:21:02 +0530

    Create service layer and test with sample data

--- src/routes/airportRoute.js
+++ src/routes/airportRoute.js
@@ -2,20 +2,26 @@
 const express = require("express")
 const router = express.Router()
 const airportURL = "/airports"
+const airportService = require("../service/airportService")
 
 router.get(airportURL,(req,res)=>{
+    airportService.getAllAirports()
     res.send("GET Airport details")
+    
 })
 
 router.post(airportURL,(req,res)=>{
+    airportService.savetAirport()
     res.send("POST Airport details")
 })
 
 router.patch(airportURL,(req,res)=>{
+    airportService.updateAirport()
     res.send("PATCH Airport details")
 })
 
 router.delete(airportURL,(req,res)=>{
+    airportService.deletelAirport()
     res.send("DELETE Airport details")
 })
 
---
+++ src/service/airportService.js
@@ -0,0 +1,18 @@
+
+async function getAllAirports () {
+    console.log("Get All Airports from Service layer")   
+}
+
+async function savetAirport () {
+    console.log("Save Airport from Service layer")
+}
+
+async function updateAirport () {
+    console.log("Update airport from Service layer")
+}
+
+async function deletelAirport () {
+    console.log("Delete Airport from Service layer")
+}
+
+module.exports = { getAllAirports, savetAirport, updateAirport, deletelAirport }
