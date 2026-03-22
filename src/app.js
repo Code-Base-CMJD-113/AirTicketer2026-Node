b107582a59edaf930c6379eaccf53acfb303f96b
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-22 13:28:41 +0530

    Add JWT secret and PORT number to .env

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
-const PORT = 3500
+const PORT = process.env.PORT || 3500
 const airportRoute = require("../src/routes/airportRoute")
 const mongoose = require("mongoose")
 app.use(express.json())
